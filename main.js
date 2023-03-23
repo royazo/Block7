cdff2713830ec487af1e565b0b0e0541d82b5a4a
Roya A <88850039+royazo@users.noreply.github.com>
2023-03-23 14:23:20 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let intro = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+//1st combo set for 10 using multiplication
+let combo1 = 5*2;
+//2nd combo set for 40 using division
+let combo2 = 80/2;
+//1st combo set for 39 using subtraction
+let combo3 = 40 - 1;
+// Concatenation of previously declared variables
+alert(intro + " " + combo1 + " - " + combo2 + " - " + combo3);
